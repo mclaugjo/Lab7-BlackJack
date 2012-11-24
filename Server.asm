10bf925a01ed9c10a6851ae5e426f32f7948bc22
devlinjunker <devlin@osu>
2012-11-23 22:03:24 -0800

    updated comments in server

--- Server.asm
+++ Server.asm
@@ -1,11 +1,13 @@
 ;***********************************************************
 ;*
-;* 	ECE 375 Lab 6 - Fall 2012
-;*	Remote.asm
+;* 	ECE 375 Lab 7 - Fall 2012
+;*	Server.asm
 ;*
-;*	This is the remote for Lab 6 of ECE 375. The Remote sends 
-;* 	one of 6 commands to a Robot: Move Forward, Move Backward, 
-;*	Turn Right, Turn Left, Halt and Send Freeze. 
+;*	This is the server for Lab 7 of ECE 375. The Server waits 
+;*	for enough players to join the game and sends each of them the 
+;*	start game signal. Once the players have all played, it determines
+;*	which player had the highest score and lets all of the players
+;*	know who won.
 ;*
 ;*
 ;***********************************************************
@@ -19,24 +21,24 @@
 ;***********************************************************
 ;*	Internal Register Definitions and Constants
 ;***********************************************************
-.def 	mpr 	= r16			; Multi-Purpose Register
-.def 	sigr	= r17			; Register to Hold Signal
-.def 	idreg	= r18			; Register to Hold Board ID
-.def 	playercnt = r19			; Register to Hold Number of Players
-.def 	timer 	= r20
+.def	mpr 	= r16			; Multi-Purpose Register
+.def	sigr	= r17			; Register to Hold Signal
+.def	idreg	= r18			; Register to Hold Board ID
+.def	winreg	= r19			; Register to Winner ID
+.def	timer 	= r20
 
-.equ 	NUM_PLAYERS = 2
+.equ	NUM_PLAYERS = 2
 
-.equ	BOARDID	= 0b00000011	; Unique Board ID = $0 3 (MSB = 0) 
+.equ	BOARDID	= 0b00000011	; Unique Board ID = $03 (MSB = 0) 
 
-.equ 	TIMEOUT = 100
+.equ	TIMEOUT = 100			; Timeout Interval
 
 ; Use these commands between the server and player (OR WITH 4 BIT BOT ID TO INCLUDE ID)
 ; MSB = 1 thus:
-.equ	NEW		= 0b10000000	;0b1000XXXX New Game Command X's will have ID
-.equ	JOIN 	= 0b10010000	;0b1001XXXX Join Game Command
-.equ	START 	= 0b10100000	;0b1010XXXX Start Game Command 
-.equ	RECIEVE	= 0b11000000	;0b1010XXXX Recieved Command Signal
+.equ	NEW		= 0b10000000	;0b1000XXXX New Game Command (OR WITH 4 BIT BOT ID TO INCLUDE ID)
+.equ	JOIN 	= 0b10010000	;0b1001XXXX Join Game Command (OR WITH 4 BIT BOT ID TO INCLUDE ID)
+.equ	START 	= 0b10100000	;0b1010XXXX Start Game Command (OR WITH 4 BIT BOT ID TO INCLUDE ID)
+.equ	RECIEVE	= 0b11000000	;0b1010XXXX Recieved Command Signal (OR WITH 4 BIT BOT ID TO INCLUDE ID)
 .equ	ASK 	= 0b10110000	;0b1011XXXX Ask for Score Command (OR WITH 4 BIT BOT ID TO INCLUDE ID)
 .equ	WINNER 	= 0b11110000	;0b1111XXXX Winner Command (OR WITH 4 BIT BOT ID TO INCLUDE ID)
 
@@ -86,9 +88,6 @@ INIT:
 		ldi mpr, $A0
 		sts UBRR1L, mpr
 
-		; Set Player Count
-		ldi playercnt, 0
-
 		; Load initial addresses of arrays
 		ldi XL, low(PLAYERS<<1)
 		ldi XH, high(PLAYERS<<1)
@@ -124,34 +123,29 @@ MAIN:
 			; Store ID in Player Array
 			st X+, idreg
 			
-			; Increment Player Count
-			inc playercnt
+			; Check If Room for More Players
+			cpi XL, low(END_PLAYERS<<1)
+			brne MAIN			; If so, Return to Main and wait for new signal
 			
-			; Check If We have Number of players specified
-			cpi playercnt, NUM_PLAYERS
-			brne MAIN			; If Not, Return to Main and wait for new signal
-			
-			rcall STARTGAME		; If so, call STARTGAME
-		
-		ldi playercnt, 0
+			rcall STARTGAME		; If not, call STARTGAME
 
 		rjmp MAIN
 
 
 STARTGAME:
 		
-		ld idreg, -X
+		ld idreg, -X			; Pre-Decrement X to return to last ID 
 
 		startLoop:
 			; Enable Transmitter
 			ldi mpr, (1<<TXEN1)
 			sts UCSR1B, mpr			
 
-			; Load Start Game Command and ServerID
-			mov mpr, idreg
-			ori mpr, START
+			mov mpr, idreg		; Move PlayerID into MPR
+			ori mpr, START 		; OR with Start Game Command
 			sts UDR1, mpr		; Send Command
-
+			
+			; Load Timeout Value to Timer Register
 			ldi timer, TIMEOUT
 		
 			; Enable Reciever
@@ -159,25 +153,26 @@ STARTGAME:
 			sts UCSR1B, mpr
 
 			checkRcvd:
-				dec timer
-				breq startLoop
-				lds mpr, UCSR1A
+				dec timer		; Decrement the Timer
+				breq startLoop	; If 0, Resend Start Game Command
+				lds mpr, UCSR1A	; Otherwise, check if Recieve Complete
 				sbrs mpr, RXC1
-				rjmp checkRcvd
+				rjmp checkRcvd	; If not, loop for Recieve Complete
 
-				lds sigr, UDR1
+				lds sigr, UDR1	; Load Recieved Signal into Signal Register
 				
-				mov mpr, idreg
-				ori mpr, RECIEVE
+				mov mpr, idreg	; Move ID we're waiting for to MPR
+				ori mpr, RECIEVE; OR with Command to get Expected Signal
 
-				cp sigr, mpr
-				brne checkRcvd
+				cp sigr, mpr	; Compare Recieved vs Expected Signal
+				brne checkRcvd	; If not equal, wait for new Recieve Complete
 			
+			; Check if Sent to All Players
 			cpi XL, low(PLAYERS<<1)
-			breq started
+			breq started		; If so, call ASK SCORES
 
-			ld idreg, -X
-			rjmp startLoop
+			ld idreg, -X		; Otherwise, Load Next ID into ID Register
+			rjmp startLoop		; And send next Start Game Command
 
 
 	started:
@@ -187,18 +182,19 @@ STARTGAME:
 		ret
 
 ASKSCORES:
-		ld idreg, X+
+		ld idreg, X+			; Load First ID them Increment X
 
 		scoreLoop:
 			; Enable Transmitter
 			ldi mpr, (1<<TXEN1)
 			sts UCSR1B, mpr			
 
-			; Load Start Game Command and ServerID
-			mov mpr, idreg
-			ori mpr, ASK
+			; Load Command to be Sent
+			mov mpr, idreg		; Move Player ID into MPR
+			ori mpr, ASK		; OR with Ask Score Command
 			sts UDR1, mpr		; Send Command
-
+			
+			; Load Timeout Value to TImer Register
 			ldi timer, TIMEOUT
 		
 			; Enable Reciever
@@ -206,24 +202,25 @@ ASKSCORES:
 			sts UCSR1B, mpr
 
 			checkScore:
-				dec timer
-				breq scoreLoop
+				dec timer		; Decrement the Timer
+				breq scoreLoop	; If 0, Resend Ask Score Command
 				lds mpr, UCSR1A
-				sbrs mpr, RXC1
-				rjmp checkScore
+				sbrs mpr, RXC1	; Otherwise, check if Recieve Complete
+				rjmp checkScore ; If not, loop for Recieve Complete
 
 				lds sigr, UDR1
 
-				sbrc sigr
-				rjmp checkScore
+				sbrc sigr, 7	; Check if MSB is cleared (Score Value)
+				rjmp checkScore	; If not, Wait for Score
 				
-			st Y+, sigr
+			st Y+, sigr			; If so, Store Score in Score Array
 			
+			; Check if Recieved All Scores
 			cpi YL, low(END_SCORES<<1)
-			breq ended
+			breq ended			; If so, call FINDWINNER
 
-			ld idreg, X+
-			rjmp scoreLoop
+			ld idreg, X+		; Otherwise, get next ID
+			rjmp scoreLoop		; And send next Ask Score Command
 
 	ended:
 		rcall FINDWINNER
@@ -232,23 +229,25 @@ ASKSCORES:
 
 
 FINDWINNER:
-		ldi sigr, 0
+		ldi sigr, 0				; Start with Best Score of 0
+		ldi winreg, 0			; Start with no Winner
 
 		winnerLoop:
-			ld idreg, -X
-			ld mpr, -Y
-			cp mpr, sigr
-			brlt keep
+			ld idreg, -X		; Load ID Register Matching Score into ID Register
+			ld mpr, -Y			; Load Score into MPR
+			cp mpr, sigr		; Compare Score with Previous Best Score
+			brlt keep			; If MPR < SIGR, keep what is in SIGR
 			
-			mov sigr, mpr
-			mov playercnt, idreg
+			mov sigr, mpr		; Otherwise, store new highest value (MPR)
+			mov winreg, idreg	; and Store Highest Scorers ID
 			
 			keep:
 
+			; Check if All Scores Checked
 			cpi idreg, low(PLAYERS<<1)
-			brne winnerLoop
+			brne winnerLoop		; If not, Loop to check next score
 			
-		rcall SENDWINNER
+		rcall SENDWINNER		; Otherwise, Send the Winner Command
 
 		ret
 
@@ -257,10 +256,10 @@ SENDWINNER:
 		ldi mpr, (1<<TXEN1)
 		sts UCSR1B, mpr			
 	
-		; Load Start Game Command and ServerID
-		mov mpr, playercnt
-		ori mpr, WINNER
-		sts UDR1, mpr		; Send Command
+		; Create and Send Winner Command
+		mov mpr, winreg			; Load Winner ID into MPR
+		ori mpr, WINNER			; OR with Winner Command
+		sts UDR1, mpr			; Send Command
 
 		ret
 
